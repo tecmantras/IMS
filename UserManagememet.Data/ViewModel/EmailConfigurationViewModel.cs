04ba0c91ff28fe4fc32b46cef2c634ee496896de
Gaurav Kumar <123172636+Gk17091997@users.noreply.github.com>
2023-07-09 13:38:11 +0530

    Email Verification

---
+++ UserManagememet.Data/ViewModel/EmailConfigurationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagememet.Data.ViewModel
+{
+    public class EmailConfigurationViewModel
+    {
+        public string SmtpType { get; set; }
+        public int SmtpPort { get; set; }
+        public string EmailSender { get; set; }
+        public string Password { get; set; }
+        public bool IsSsl { get; set; }
+    }
+}
